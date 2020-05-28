4865ecb4a1bf65e8b3b1818bbd38c10ee408948a
BuildTools <unconfigured@null.spigotmc.org>
2020-05-28 18:22:58 +1000

    update

--- rmit-bot.py
+++ rmit-bot.py
@@ -91,29 +91,27 @@ async def archivecourse(ctx, cat_id):
 @bot.command(aliases=['link'])
 async def links(ctx, *, arg):
 	arg = arg.lower()
-	await ctx.send('1')
 	field_value_1 = ''
 	field_value_2 = ''
+	footer = 'All links sourced from RMIT official website https://rmit.edu.au/'
 	if arg == 'student support':
 		description = 'Student Support'
 		field_value_1 = 'https://www.rmit.edu.au/students/support-and-facilities/student-support'
-		await ctx.send('2')
 	elif arg == 'quick links' or arg == 'quicklinks':
-		description = 'Quick Links'
+		description = 'Quick Links - Here are the systems and resources you use often.'
 		field_value_1 = '[myRMIT](https://my.rmit.edu.au/portal/)\n[RMIT Creds](https://www.rmit.edu.au/creds)\n[Canvas](https://rmit.instructure.com/)\n[Library](https://www.rmit.edu.au/library)\n[Study help](https://www.rmit.edu.au/students/study-support)\n[myDesktop](https://mydesktop.rmit.edu.au/)'
 		field_value_2 = '[Enrolment Online](https://sams.rmit.edu.au/)\n[Blackboard (LMS)](https://lms.rmit.edu.au/)\n[Student email](https://www.rmit.edu.au/students/support-and-facilities/it-services-for-students/email)\n[myTimetable](https://www.rmit.edu.au/students/student-essentials/program-and-course-information/class-timetables/access-mytimetable)\n[RMIT Connect](https://rmit.service-now.com/connect/?id=rmit_index)'
-		await ctx.send('2')
 		
 	links_embed = discord.Embed(
 		title = 'RMIT Links',
 		description = description,
 		colour = 0xE00303
 		)
-	await ctx.send('3')
 	if field_value_1 != '':
 		links_embed.add_field(name='Link', value=field_value_1)
 	if field_value_2 != '':
 		links_embed.add_field(name='Link', value=field_value_2)
+	links_embed.set_footer(text=footer, icon_url='https://www.rmit.edu.au/etc/designs/rmit-student-portal/clientlib/css/images/rmit_logo_blue.svg')
 	await ctx.send(embed=links_embed)
 
 @bot.command(aliases=['findbuilding'])
